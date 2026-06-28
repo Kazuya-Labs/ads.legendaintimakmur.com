8550ece4768204cac8cbf0b44f7865a9ddd0041b
Kazuya-Labs <zuyabizon01@gmail.com>
2026-06-28 22:22:14 +0700

    ini

---
+++ prettierrc.mjs
@@ -0,0 +1,14 @@
+import * as prettierPluginAstro from "prettier-plugin-astro";
+
+/** @type {import("prettier").Config} */
+export default {
+  plugins: [prettierPluginAstro],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
